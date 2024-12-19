8c1b31b1d75d502010e9cee20a32374846eaec2d
Omar Khalili <OmarKhalili810@gmail.com>
2024-12-19 13:11:02 +0200

    creating Data definition language

---
+++ DDL.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Books(
+	BookID int PRIMARY KEY,
+	Title varchar(255) NOT NULL,
+	Author varchar(255),
+	ISBN int UNIQUE NOT NULL,
+	[Published Date] Date,
+	Genre varchar(255),
+	[Shelf Location] varchar(510),
+	[Current Status] varchar(10)
+);
+
+CREATE TABLE Borrowers(
+	BorrowerID int NOT NULL,
+	[First Name] varchar(255),
+	[Last Name] varchar(255),
+	Email varchar(255),
+	[Date of birth] date,
+	[Membership date] date,
+	PRIMARY KEY(BorrowerID)
+);
+
+CREATE TABLE Loans(
+	LoanID int PRIMARY KEY,
+	BorrowerID int FOREIGN KEY REFERENCES Borrowers(BorrowerID),
+	BookID int,
+	[Date Borrowed] date,
+	[Due Date] date,
+	[Date Returned] date DEFAULT NULL,
+	FOREIGN KEY (BookID) REFERENCES Books(BookID)
+);
