a307c6ef6b43042c2dd51e3c098432ac8eb17b83
Omar Khalili <OmarKhalili810@gmail.com>
2024-12-20 13:15:43 +0200

    adding another way to done the requierment with more details about the borrower

--- ActiveBorrowersWithCTEs.sql
+++ ActiveBorrowersWithCTEs.sql
@@ -14,3 +14,22 @@ FROM BorrowerCounts b
 JOIN ActiveBorrowers a ON b.BorrowerID = a.BorrowerID
 GROUP BY b.BorrowerID
 HAVING COUNT(a.BookID) >= 2;
+
+
+;with BorrowerIdentifyer as (
+	SELECT BorrowerID
+	,COUNT(*)
+	borrowCount from Loans
+	where [Date Returned] IS NULL
+	GROUP BY BorrowerID
+),
+ActiveBorrowers as (
+	select *
+	from Borrowers
+	where BorrowerID
+	IN
+	(select BorrowerID from BorrowerIdentifyer where borrowCount>=2)
+)
+select * from ActiveBorrowers;
+
+
